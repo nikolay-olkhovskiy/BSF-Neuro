c0c2e9871d4ff3db597b24ce6a5dcf5f31938f8b
Leonid Sokolinsky <Leonid.Sokolinsky@susu.ru>
2020-08-09 16:14:17 +0500

    Add files via upload

--- Problem-Parameters.h
+++ Problem-Parameters.h
@@ -7,7 +7,6 @@ Author: Leonid B. Sokolinsky
 
 This source code is a part of BSF Skeleton
 ==============================================================================*/
-#pragma once
 
 //=========================== Problem Parameters =========================
 
